dcda08f72decf84ca543e0755a620d3a72c39774
Yin Song <yinsong1986@gmail.com>
2016-05-11 22:43:46 +1000

    Add the function and unit test of scoring a frame.

--- src/bowling_scorecard/core.clj
+++ src/bowling_scorecard/core.clj
@@ -6,6 +6,17 @@
   []
   (vector))
 
+(defn score-a-frame
+  "Given a score card, score a frame"
+  ;; three balls for special last frame
+  ([score_card, result_of_ball_1, result_of_ball_2, result_of_ball_3]
+     (if (and (= "x" result_of_ball_1) (= 9 (count score_card)))
+       (conj score_card [result_of_ball_1 result_of_ball_2 result_of_ball_3])
+       nil))
+  ;; two balls for other cases
+  ([score_card, result_of_ball_1, result_of_ball_2]
+     (conj score_card [result_of_ball_1 result_of_ball_2])))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/bowling_scorecard/core_test.clj
+++ test/bowling_scorecard/core_test.clj
@@ -5,3 +5,11 @@
 (deftest new-scorecard-test
   (testing "creating a new empty scorecard"
     (is (= [] (new-scorecard)))))
+
+(deftest score-a-frame-test
+  (testing "Score the first frame."
+    (is (= [["7" "/"]] (score-a-frame [] "7" "/"))))
+  (testing "Score the correct last frame."
+    (is (= [["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x" "7" "/"]] (score-a-frame [["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"]] "x" "7" "/"))))
+  (testing "Return nil for the wrong last frame."
+    (is (= nil (score-a-frame [["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"]] "3" "7" "/")))))
