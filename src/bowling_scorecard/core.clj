6b457f5cd250942ae4d62bb07f6f6d3c1b81c98b
Yin Song <yinsong1986@gmail.com>
2016-05-11 19:12:58 +1000

    Implement the function and unit test of creating an empty score card.

--- src/bowling_scorecard/core.clj
+++ src/bowling_scorecard/core.clj
@@ -1,6 +1,11 @@
 (ns bowling-scorecard.core
   (:gen-class))
 
+(defn new-scorecard
+  "Create an empty score card"
+  []
+  (vector))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/bowling_scorecard/core_test.clj
+++ test/bowling_scorecard/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [bowling-scorecard.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest new-scorecard-test
+  (testing "creating a new empty scorecard"
+    (is (= [] (new-scorecard)))))
