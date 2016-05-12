86df5afe41b8e5cc3d1620fa5be130b219238603
Yin Song <yinsong1986@gmail.com>
2016-05-13 00:45:51 +1000

    Add functions and unit tests related to the check if the game is complete.

--- src/bowling_scorecard/core.clj
+++ src/bowling_scorecard/core.clj
@@ -17,6 +17,69 @@
   ([score_card, result_of_ball_1, result_of_ball_2]
      (conj score_card [result_of_ball_1 result_of_ball_2])))
 
+(defn intize-balls
+  "Change the raw score format of balls in a frame, e.g., x, / to integer score"
+  [balls]
+  (remove
+    nil?
+    (vector 
+      (if (= (get balls 0) "x") 10 (read-string (get balls 0)))
+      (if (= (get balls 1) nil)
+        nil
+        (cond
+          (= (get balls 1) "x") 10
+          (= (get balls 1) "/") (- 10 (read-string (get balls 0)))
+          :else (read-string (get balls 1))))
+      (if (= (get balls 2) nil)
+        nil
+        (cond
+          (= (get balls 2) "x") 10
+          (= (get balls 2) "/") (- 10 (read-string (get balls 1)))
+          :else (read-string (get balls 2)))))))
+
+(defn strike? [balls]
+  "Check if it is a strike."
+  (= 10 (first balls)))
+
+(defn spare? [balls]
+  "Check if it is a spare."
+  (= 10 (apply + (take 2 balls))))
+
+(defn balls-a-frame-score
+  "The number of balls contribute to a frame's score."
+  [balls]
+  (cond
+   (strike? balls) 3
+   (spare? balls) 3
+   :else 2))
+
+(defn balls-a-frame
+  "The number of balls in a frame"
+  [balls]
+  (if (strike? balls) 1 2))
+
+(defn final-frame-scores
+  "Count the final scores of each frame"
+  [balls_seq]
+  (if (empty? balls_seq)
+    [0]
+    (concat (vector (reduce + (take (balls-a-frame-score balls_seq) balls_seq)))
+      (final-frame-scores (drop (balls-a-frame balls_seq) balls_seq)))))
+
+(defn final-score
+  "Count the overall score"
+  [frame_scores]
+  (reduce + (take 10 frame_scores)))
+
+(defn check-complete
+  "Determine whether a game is complete. If so, provide the final score"
+  [score_card]
+  (if (= 10 (count score_card))
+    (final-score
+      (final-frame-scores
+        (flatten (map intize-balls score_card))))
+    nil))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/bowling_scorecard/core_test.clj
+++ test/bowling_scorecard/core_test.clj
@@ -13,3 +13,78 @@
     (is (= [["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x" "7" "/"]] (score-a-frame [["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"]] "x" "7" "/"))))
   (testing "Return nil for the wrong last frame."
     (is (= nil (score-a-frame [["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"]] "3" "7" "/")))))
+
+(deftest intize-balls-test
+  (testing "Transform a 3-ball frame to integers."
+    (is (= [10 7 3] (intize-balls ["x" "7" "/"])))
+    (is (= [10 7 2] (intize-balls ["x" "7" "2"]))))
+  (testing "Transform a 2-ball frame to integers."
+    (is (= [7 3] (intize-balls ["7" "/"])))
+    (is (= [7 2] (intize-balls ["7" "2"]))))
+  (testing "Transform a 1-ball frame to integers."
+    (is (= [10] (intize-balls ["x"])))))
+
+(deftest strike?-test
+  (testing "check if is a strike"
+    (is (= true (strike? [10 3 4])))
+    (is (= false (strike? [5 3 4])))))
+
+(deftest spare?-test
+  (testing "check if is a spare"
+    (is (= true (spare? [7 3 4])))
+    (is (= false (spare? [5 3 4])))))
+
+(deftest balls-a-frame-score-test
+  (testing "Count the balls contributing to a frame's score."
+    (is (= 3 (balls-a-frame-score [10 3 4 5])))
+    (is (= 3 (balls-a-frame-score [7 3 4 5])))
+    (is (= 2 (balls-a-frame-score [5 3 4 5])))))
+
+(deftest balls-a-frame-test
+  (testing "Count the balls of a frame."
+    (is (= 1 (balls-a-frame [10 3 4 5])))
+    (is (= 2 (balls-a-frame [7 3 4 5])))
+    (is (= 2 (balls-a-frame [5 3 4 5])))))
+
+(deftest final-frame-scores-test
+  (testing "Count the final score of each frame."
+    (is (= [20 17 9 20 30 22 15 5 17 13 3 0]
+           (final-frame-scores (flatten [[10]
+                                  [7 3]
+                                  [7 2]
+                                  [9 1]
+                                  [10]
+                                  [10]
+                                  [10]
+                                  [2 3]
+                                  [6 4]
+                                  [7 3 3]]))))))
+
+(deftest final-score-test
+  (testing "Count the final score."
+    (is (= 168
+           (final-score [20 17 9 20 30 22 15 5 17 13 3 0])))))
+
+(deftest check-complete-test
+  (testing "Check if the game is compelte."
+    (is (= 168
+           (check-complete [["x"]
+                           ["7" "/"]
+                           ["7" "2"]
+                           ["9" "/"]
+                           ["x"]
+                           ["x"]
+                           ["x"]
+                           ["2" "3"]
+                           ["6" "/"]
+                           ["7" "/" "3"]])))
+    (is (= nil
+           (check-complete [["x"]
+                           ["7" "/"]
+                           ["7" "2"]
+                           ["9" "/"]
+                           ["x"]
+                           ["x"]
+                           ["x"]
+                           ["2" "3"]
+                           ["6" "/"]])))))
